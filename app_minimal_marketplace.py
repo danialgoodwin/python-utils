81dbc41d9cd39d836e969759725cc08b81daad28
Danial Goodwin <danialgoodwin@gmail.com>
2020-02-03 23:49:19 -0700

    Create app_minimal_marketplace.py

---
+++ app_minimal_marketplace.py
@@ -0,0 +1,19 @@
+# Minimal code needed to run a marketplace.
+#
+
+
+class Product:
+    pass
+
+
+class User:
+    products
+    def sell(product: Product):
+
+
+def main():
+    print('main()')
+
+
+if __name__ == '__main__':
+    main()
